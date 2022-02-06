83d5a3a62120d0cda3a7a01ef39f959acc8eb012
mcstewart76 <mcstewart76@gmail.com>
2022-02-06 01:17:56 -0500

    Started api routes file system

---
+++ routes/api/notes.js
@@ -0,0 +1,10 @@
+// GET * should return the index.html
+
+// GET /notes returning notes.html file
+
+//GET /api/notes read db.json and return all saved notes as JSON
+
+//POST /api/notes /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. 
+//You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
+
+//DELETE /api/notes/:id
