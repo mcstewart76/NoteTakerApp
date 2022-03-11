931ee853154d8762ba6b4eadad697e690db2033e
mcstewart76 <mcstewart76@gmail.com>
2022-03-10 21:48:24 -0500

    Added helper file for js helper, added notes route to use helper and get and post

--- routes/api/notes.js
+++ routes/api/notes.js
@@ -1,20 +1,43 @@
-const router = require('express').Router();
+const notes = require('express').Router();
 const path = require('path');
+const { v4: uuidv4 } = require('uuid');
+const {
+    readFromFile,
+    readAndAppend,
+    writeToFile,
+  } = require('../../helpers/fsUtils');
 
 // AT /api/notes
 
 //GET /api/notes read db.json and return all saved notes as JSON
-router.get('/', (req, res) => {
-
+notes.get('/', (req, res) => {
+    
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+      
 })
-//POST /api/notes /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. 
+//POST  /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. 
 //You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
-router.post('/', (req, res) => {
+notes.post('/', (req, res) => {
+    console.log(req.body);
 
+  const { title, text } = req.body;
+
+  if (req.body) {
+    const newNote = {
+      title,
+      text,
+      id: uuidv4(),
+    };
+
+    readAndAppend(newNote, './db/db.json');
+    res.json(`Note added successfully`);
+  } else {
+    res.error('Error in adding note');
+  }
 })
 //DELETE /api/notes/:id
-router.delete('/:id', (req, res) => {
+notes.delete('/:id', (req, res) => {
 
 })
 
-module.exports = router;
+module.exports = notes;
